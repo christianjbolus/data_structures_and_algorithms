6c42381d192ad74eb71deb766a3fcc7646b799eb
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-09 11:56:49 -0400

    Define private helper methods

--- stacks_and_queues/bracket_validator/bracketValidator.ts
+++ stacks_and_queues/bracket_validator/bracketValidator.ts
@@ -18,4 +18,16 @@ class BracketValidator {
   constructor(public stack: Stack) {
     this.stack = new Stack()
   }
+
+  private isOpeningBracket(char: string): boolean {
+    return ['{', '[', '('].includes(char)
+  }
+
+  private isClosingBracket(char: string): boolean {
+    return ['}', ']', ')'].includes(char)
+  }
+
+  private isBracketMatch(openingBracket: string, char: string): boolean {
+    return char === { '{' : '}', '[': ']', '(': ')'}[openingBracket]
+  }
 }
