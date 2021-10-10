ff214fd37ea07351c109a2ca616d25e4d66ce766
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-10 11:18:54 -0400

    Factor out Stack class into separate file

--- stacks_and_queues/bracket_validator/src/BracketValidator.ts
+++ stacks_and_queues/bracket_validator/src/BracketValidator.ts
@@ -1,18 +1,4 @@
-class Stack {
-  stack: string[] = [];
-
-  add(str: string) {
-    this.stack.push(str);
-  }
-
-  remove(): string {
-    return this.stack.pop();
-  }
-
-  read(): string | undefined {
-    return this.stack[this.stack.length - 1];
-  }
-}
+import { Stack } from './Stack'
 
 export class BracketValidator {
   stack: Stack
